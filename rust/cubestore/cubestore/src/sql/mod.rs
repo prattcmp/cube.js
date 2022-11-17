bc718a8c5038e5f7524cf3e384c32d29f80d7231
Dmitry Patsura <talk@dmtry.me>
2022-11-17 16:18:53 +0300

    chore: fix testing

--- rust/cubestore/cubestore/src/sql/mod.rs
+++ rust/cubestore/cubestore/src/sql/mod.rs
@@ -1725,7 +1725,10 @@ mod tests {
     async fn create_schema_test() {
         let config = Config::test("create_schema_test");
         let path = "/tmp/test_create_schema";
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
+
         let store_path = path.to_string() + &"_store".to_string();
         let remote_store_path = path.to_string() + &"remote_store".to_string();
         let _ = fs::remove_dir_all(store_path.clone());
@@ -1781,7 +1784,9 @@ mod tests {
                 ])
             );
         }
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
         let _ = fs::remove_dir_all(store_path.clone());
         let _ = fs::remove_dir_all(remote_store_path.clone());
     }
@@ -1790,7 +1795,10 @@ mod tests {
     async fn create_table_test() {
         let config = Config::test("create_table_test");
         let path = "/tmp/test_create_table";
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
+
         let store_path = path.to_string() + &"_store".to_string();
         let remote_store_path = path.to_string() + &"remote_store".to_string();
         let _ = fs::remove_dir_all(store_path.clone());
@@ -1873,7 +1881,10 @@ mod tests {
                 TableValue::String("NULL".to_string()),
             ]));
         }
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
+
         let _ = fs::remove_dir_all(store_path.clone());
         let _ = fs::remove_dir_all(remote_store_path.clone());
     }
@@ -1882,7 +1893,10 @@ mod tests {
     async fn create_table_test_seal_at() {
         let config = Config::test("create_table_test_seal_at");
         let path = "/tmp/test_create_table_seal_at";
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
+
         let store_path = path.to_string() + &"_store".to_string();
         let remote_store_path = path.to_string() + &"remote_store".to_string();
         let _ = fs::remove_dir_all(store_path.clone());
@@ -1965,7 +1979,10 @@ mod tests {
                 TableValue::String("NULL".to_string()),
             ]));
         }
-        let _ = DB::destroy(&Options::default(), path);
+
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("metastore"));
+        let _ = DB::destroy(&Options::default(), Path::new(path).join("cachestore"));
+
         let _ = fs::remove_dir_all(store_path.clone());
         let _ = fs::remove_dir_all(remote_store_path.clone());
     }
